c9d4f33247a67f173d90f3118dde14f490052df1
Julian David <julianfullstackdev@gmail.com>
2023-05-02 21:55:47 -0500

    add nativewind, react navigation and stacks navigation

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text className="text-red-500">HomeScreen</Text>
+    </View>
+  );
+}
