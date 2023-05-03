ba9b4eda8183e96afb69d4a5f70280d4d46373d4
Julian David <julianfullstackdev@gmail.com>
2023-05-02 22:09:41 -0500

    add lucide icons, start homescreen header

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,33 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500"></Text>
+
+      <View className="flex-row pb-3 mx-4 items-center space-x-3">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 rounded-full"
+        />
+
+        <View>
+          <Text className="font-bold text-gray-400 text-xs">Deliver now</Text>
+          <Text className="font-bold text-xl">Current location</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
