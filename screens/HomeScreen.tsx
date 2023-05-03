43ea9c751c34e5f97cef12d9fe6e470be968b066
Julian David <julianfullstackdev@gmail.com>
2023-05-02 22:17:36 -0500

    advance in header

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { ChevronDown, User } from "lucide-react-native";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -12,7 +13,7 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       <Text className="text-red-500"></Text>
 
       <View className="flex-row pb-3 mx-4 items-center space-x-3">
@@ -23,10 +24,15 @@ export default function HomeScreen() {
           className="h-7 w-7 bg-gray-300 rounded-full"
         />
 
-        <View>
+        <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Deliver now</Text>
-          <Text className="font-bold text-xl">Current location</Text>
+          <Text className="font-bold text-xl">
+            Current location
+            <ChevronDown color="#00CCBB" />
+          </Text>
         </View>
+
+        <User size={25} color="#00CCBB" />
       </View>
     </SafeAreaView>
   );
