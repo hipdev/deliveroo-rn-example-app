cade962aec445376c34d7179f8065ed8ca04c6b3
Julian David <julianfullstackdev@gmail.com>
2023-05-08 07:18:52 -0500

    add currency formatter

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ or whatever seems to date at your time reading this xD.
 * At 1:26 Sonny recommends to install sanity cli, that's not needed now in v3
 * I'm not using Hero icons, instead I'm using Lucide Icons
 * I'm using TypeScript instead too, to generate types I'm using the [sanity-codegen](https://www.sanity.io/plugins/sanity-codegen) package, everything is typed.
+* I'm using react-native-format-currency, is a stable, lightweight and not dead package as a replacement for react-currency-formatter
 
 
 ### If you get an error at 1:38 trying to get the Sanity data, please
--- components/DishRow.tsx
+++ components/DishRow.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Dish } from '../sanity/types/schema'
 
+import { formatCurrency } from 'react-native-format-currency'
+
 const DishRow = (dish: Dish) => {
+  const formattedPrice = formatCurrency({
+    amount: dish.price || 0,
+    code: 'GBP',
+  })
   return (
-    <View>
-      <Text>DishRow</Text>
-    </View>
+    <TouchableOpacity>
+      <View>
+        <Text>{dish.name}</Text>
+        <Text>{dish.short_description}</Text>
+        <Text>{formattedPrice}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
