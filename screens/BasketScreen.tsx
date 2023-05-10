3bbedf268dc319e12a441af91d85f56b4fbf7dce
Julian David <julianfullstackdev@gmail.com>
2023-05-09 23:06:34 -0500

    advance in basket screen

--- screens/BasketScreen.tsx
+++ screens/BasketScreen.tsx
@@ -1,8 +1,12 @@
-import { View, Text } from 'react-native'
-import React, { useMemo, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { useRestaurantStore } from '../stores/restaurantStore'
+
+import { XCircle } from 'lucide-react-native'
+import React, { useMemo, useState } from 'react'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
 import { useBasketStore } from '../stores/basketStore'
+import { useRestaurantStore } from '../stores/restaurantStore'
 
 const BasketScreen = () => {
   const navigation = useNavigation()
@@ -23,9 +27,39 @@ const BasketScreen = () => {
   console.log(restaurant, 'restaurant', groupedItemsInBasket)
 
   return (
-    <View>
-      <Text>BasketScreen</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-white'>
+      <View className='flex-1 bg-gray-100'>
+        <View className='p-5 border-b border-primary bg-white shadow-xs'>
+          <View>
+            <Text className='text-lg font-bold text-center'>Basket</Text>
+            <Text className='text-center text-gray-400'>{restaurant.name}</Text>
+          </View>
+
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className='rounded-full bg-gray-100 absolute top-3 right-5'
+          >
+            <XCircle className='text-primary' height={50} width={50} />
+          </TouchableOpacity>
+        </View>
+
+        {/* Image */}
+
+        <View className='flex-row items-center space-x-4 px-4 py-3 bg-white my-5'>
+          <Image
+            source={{ uri: 'https://links.papareact.com/wru' }}
+            className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+          />
+
+          <Text className='flex-1'>Deliver in 50-75 min</Text>
+          <TouchableOpacity>
+            <Text className='text-primary'>Change</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Items in the basket */}
+      </View>
+    </SafeAreaView>
   )
 }
 
